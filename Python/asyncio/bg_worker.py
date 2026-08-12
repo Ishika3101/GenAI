b997dbd849003d514b87785560de92fa74b53635
Ishika Goyal <goyal.ishika6789@gmail.com>
2026-08-12 17:23:23 +0530

    daemon

--- Python/asyncio/bg_worker.py
+++ Python/asyncio/bg_worker.py
@@ -2,16 +2,17 @@ import asyncio
 import threading
 import time
 
+#we are running are application in asynchronous mode but also app has separate thread that just logs system health
 def background_worker():
     while True:
         time.sleep(1)
         print(f"Logging the system health 🕰️")
 
 async def fetch_orders():
-    await asyncio.sleep(3)
+    await asyncio.sleep(3)  #time.sleep will not interfere this sleep
     print("🎁 order fetched")
 
-
+#first get the thread ready
 threading.Thread(target=background_worker, daemon=True).start()
 
 asyncio.run(fetch_orders())
