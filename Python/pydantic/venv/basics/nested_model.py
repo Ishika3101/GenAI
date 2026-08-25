c7307bd678268ebc28b4a70049c6aefbd5f8c474
Ishika Goyal <goyal.ishika6789@gmail.com>
2026-08-25 17:13:14 +0530

    nested model

--- Python/pydantic/venv/basics/nested_model.py
+++ Python/pydantic/venv/basics/nested_model.py
@@ -11,3 +11,29 @@ class User(BaseModel):
     id:int
     name:str
     address:Address #address is type of address which is already defined above so this is nested
+
+
+address=Address(
+    street="sector 73",
+    city="gurgaon",
+    postal_code="122004"
+)
+
+user=User(
+    id=1,
+    name='ishika',
+    address=address
+)
+#or
+user_data={
+    'id':1,
+    'name':'ishika',
+    'address':{
+        'street':"sector 73",
+        'city':"gurgaon",
+        'postal_code':"122004"
+    }
+}
+
+user=User(**user_data)
+print(user)
